b65c88503f6aaa252e828615126459a015f9236b
Shakib448 <shakiba448@gmail.com>
2020-08-07 09:42:17 +0600

    first commit

---
+++ Null-Vs-Undefined.js
@@ -0,0 +1,25 @@
+let name;
+console.log(name);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const love = {
+    name: "Smart Dude",
+    phone: 32451
+}
+
+const result = add(13);
+console.log(love.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 4, 5];
+console.log(ages)
