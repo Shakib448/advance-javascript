172be57bc3b4fd247c825eca0406d454e4dc7101
Shakib448 <shakiba448@gmail.com>
2020-08-07 10:00:31 +0600

    First commit

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+const first = 0;
+const second = false;
+
+if (first === second) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+// == means check value
+// === means check value + type
